2454bb66979bb8f4d176c4c4a0f813c7171268ad
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-12-04 13:06:33 +0000

    class info

--- src/indonesia_potential_biomass.py
+++ src/indonesia_potential_biomass.py
@@ -32,4 +32,22 @@ NetCDF_file = '/disk/scratch/local.2/southeast_asia_PFB/southeast_asia_PFB_mean_
 ForestCover_file = '/home/dmilodow/DataStore_DTM/FOREST2020/PartnerCountries/Indonesia/ForestCover/Primary_and_intact_forest_and_loss/change/margono_indonesia_forestcover_and_loss_1km.tif'
 
 agb_ds,geoTrans1 = EO.load_NetCDF(NetCDF_file,lat_var = 'lat', lon_var = 'lon')
-forestclass, geoTrans2, coord_sys = io.load_GeoTIFF_band_and_georeferencing(File)
+forestclass, geoTrans2, coord_sys = io.load_GeoTIFF_band_and_georeferencing(ForestCover_file)
+
+# Forest Classes:
+# 0   - Out of area study
+# 1   - No change of primary degraded forest from 2000-2012
+# 2   - No change of primary intact forest from 2000-2012
+# 3   - No change of non-primary from 2000-2012
+# 4   - Primary intact, cleared 2005
+# 5   - Primary intact, cleared 2010
+# 6   - Primary intact, cleared 2012
+# 7   - Primary intact, degraded 2005
+# 8   - Primary intact, degraded 2010
+# 9   - Primary intact, degraded 2012
+# 10  - Primary degraded, cleared 2005
+# 11  - Primary degraded, cleared 2010
+# 12  - Primary degraded, cleared 2012
+# 13  - Primary intact degraded 2005, cleared 2010
+# 14  - Primary intact degraded 2005, cleared 2012
+# 15  - Primary intact degraded 2010, cleared 2012
