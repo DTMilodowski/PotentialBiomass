5b683f9d1add56f1a0437b2ab43a82050c5482f8
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-12-04 13:30:45 +0000

    finishing up for now - now calculating agb, pot and seq for classes. needs checking and analysis

--- src/indonesia_potential_biomass.py
+++ src/indonesia_potential_biomass.py
@@ -31,8 +31,25 @@ SAVEDIR = '/home/dmilodow/DataStore_DTM/'
 NetCDF_file = '/disk/scratch/local.2/southeast_asia_PFB/southeast_asia_PFB_mean_WorldClim2.nc'
 ForestCover_file = '/home/dmilodow/DataStore_DTM/FOREST2020/PartnerCountries/Indonesia/ForestCover/Primary_and_intact_forest_and_loss/change/margono_indonesia_forestcover_and_loss_1km.tif'
 
-agb_ds,geoTrans1 = EO.load_NetCDF(NetCDF_file,lat_var = 'lat', lon_var = 'lon')
+ds,geoTrans1 = EO.load_NetCDF(NetCDF_file,lat_var = 'lat', lon_var = 'lon')
 forestclass, geoTrans2, coord_sys = io.load_GeoTIFF_band_and_georeferencing(ForestCover_file)
+
+# Calculate reforestation sequestration potential
+cell_areas = ds.variables['areas']
+
+nodata_mask = ds.variables['AGB_mean']==-9999
+
+agb_ds = ds.variables['AGB_mean'][:]*cell_areas/10.**4 # convert carbon density to total carbon. Note conversion from m2 to ha
+agb_ds[nodata_mask] = np.nan
+agbpot_ds = ds.variables['AGBpot_mean'][:]*cell_areas/10.**4 # convert carbon density to total carbon. Converting from m2 to ha
+agbpot_ds[nodata_mask] = np.nan
+
+forests_ds = ds.variables['forests'][:]
+seqpot_ds = agbpot_ds-agb_ds
+
+# tidy up seqpot
+seqpot_ds[forests_ds==1] = 0.
+seqpot_ds[seqpot_ds<0] = 0.
 
 # Forest Classes:
 # 0   - Out of area study
@@ -51,3 +68,20 @@ forestclass, geoTrans2, coord_sys = io.load_GeoTIFF_band_and_georeferencing(Fore
 # 13  - Primary intact degraded 2005, cleared 2010
 # 14  - Primary intact degraded 2005, cleared 2012
 # 15  - Primary intact degraded 2010, cleared 2012
+
+forestclass[forestclass==0]=np.nan
+classes = np.unique(forestclass[~np.isnan(forestclass)])
+classes = classes[~np.isnan(
+nclass = classes.size
+
+agb = np.zeros(15)
+agbpot = np.zeros(15)
+seqpot = np.zeros(15)
+
+for cc in range(0,nclass):
+    mask = forestclass==cc+1
+    agb[cc] = np.sum(agb_ds[mask])
+    agbpot[cc] = np.sum(agb_ds[mask])
+    seqpot[cc] = np.sum(seqpot_ds[mask])
+    
+    
