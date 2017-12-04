d35fb7568cff00aaba5b20d977615e538bcba9f5
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-12-04 13:02:23 +0000

    correcting paths

--- src/indonesia_potential_biomass.py
+++ src/indonesia_potential_biomass.py
@@ -12,6 +12,8 @@ import prepare_EOlab_layers as EO
 sys.path.append('/home/dmilodow/DataStore_DTM/FOREST2020/PotentialBiomass/src')
 import geospatial_utility_tools as geo
 
+sys.path.append('/home/dmilodow/DataStore_DTM/FOREST2020/EOdata/EO_data_processing/src/')
+import data_io as io
 
 # Get perceptionally uniform colourmaps
 sys.path.append('/home/dmilodow/DataStore_DTM/FOREST2020/EOdata/EO_data_processing/src/plot_EO_data/colormap/')
@@ -22,8 +24,8 @@ plt.register_cmap(name='plasma', cmap=cmaps.plasma)
 plt.register_cmap(name='magma', cmap=cmaps.magma)
 plt.set_cmap(cmaps.viridis)
 
-plt.figure(1, facecolor='White',figsize=[2, 1])
-plt.show()
+#plt.figure(1, facecolor='White',figsize=[2, 1])
+#plt.show()
 
 SAVEDIR = '/home/dmilodow/DataStore_DTM/'
 NetCDF_file = '/disk/scratch/local.2/southeast_asia_PFB/southeast_asia_PFB_mean_WorldClim2.nc'
