403276d01e06966f44e5b0291c783274da9bdb1b
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-12-15 16:35:22 +0000

    sankey script ready togo

--- src/sankey.py
+++ src/sankey.py
@@ -39,8 +39,6 @@ plt.register_cmap(name='plasma', cmap=cmaps.plasma)
 plt.register_cmap(name='magma', cmap=cmaps.magma)
 plt.set_cmap(cmaps.viridis)
 
-
-
 # This is the plotting script
 def plot_sankey(axis,A,x_locs = None,colours = None,colourmap = None,patch_width=None):
     
@@ -71,8 +69,9 @@ def plot_sankey(axis,A,x_locs = None,colours = None,colourmap = None,patch_width
     if x_locs not in locals():
         x_locs = np.arange(n_steps)+1.
     if patch_width_fraction not in locals():
+        # default patch width is 10% of average interval
         path_width_fraction=0.1
-    patch_width = path_width_fraction*(x_locs[0]-x_locs[-1]/float(n_steps)) # default patch width is 10% of average interval
+    patch_width = path_width_fraction*(x_locs[0]-x_locs[-1]/float(n_steps)) 
         
     # loop through timesteps and fill in class abundances and path details
     for cc in range(0,n_class):
@@ -85,32 +84,65 @@ def plot_sankey(axis,A,x_locs = None,colours = None,colourmap = None,patch_width
 
     # Now get the cumsum of the classes and paths so that we can plot the sankey
     # diagram
-    class_abundance_cum = np.cumsum(class_abundance,axis=0)
-    paths_i_cum = np.cumsum(paths_i,axis=0)
-    paths_f_cum = np.cumsum(paths_f,axis=0)
+    class_ulim = np.cumsum(class_abundance,axis=0)
+    class_llim = np.zeros(class_ulim.shape)
+    class_llim[1:,:] = class_ulim[:-1,:]
+    
+    paths_i_ulim = np.cumsum(paths_i,axis=0)
+    paths_i_llim = np.zeros(paths_i_ulim.shape)
+    paths_i_llim[1:,:] = paths_i_ulim[:-1,:]
+    
+    paths_f_ulim = np.cumsum(paths_f,axis=0)
+    paths_f_llim = np.zeros(paths_f_ulim.shape)
+    paths_f_llim[1:,:] = paths_f_ulim[:-1,:]
 
-    # Now we have everything that we need to plot the diagram
+    # Now we have everything that we need to plot the diagram - first up the columns
     # Loop through the number of steps
     patches = []
     for tt in range(0,n_steps):
         # Loop through each class, and create a polygon patch for each class
-        class_llim = np.zeros(n_class)
-        class_llim[1:] = class_abundance_cum[:-1,tt]
-        class_ulim = class_abundance_cum[:,tt]
-
         x_l = x_locs[tt]-patch_width/2.
         x_r = x_locs[tt]+patch_width/2.
 
         for cc in range(0, n_class):
-            UL = [x_l,class_ulim]
-            UR = [x_r,class_ulim]
-            LR = [x_r,class_llim]
-            LL = [x_l,class_llim]
+            UL = [x_l,class_ulim[cc,tt]]
+            UR = [x_r,class_ulim[cc,tt]]
+            LR = [x_r,class_llim[cc,tt]]
+            LL = [x_l,class_llim[cc,tt]]
             box = np.array([UL,UR,LR,LL])
             polygon = Polygon(box,True,ec='0.5',fc=colours[cc])
             patches.append(polygon)
             
     p_class = PatchCollection(patches, match_original=True)
     axis.add_collection(p_class)
-    
+
+    # Now we add the paths. These are going to be curved because it looks a lot nicer
+    # Will use a logistic function, which has a parameters:
+    # L = vertical difference between coordinates to be joined
+    # x0 = midpoint
+    # k = steepness of curve, which we start with as k=1
+    k=1.
+    for tt in range(0,n_steps-1):
+        t1 = x_locs[tt]
+        t2 = x_locs[tt+1]
+
+        # loop through the paths.
+        x = np.arange(t1,t2,0.01)
+        x0 = (t2+t1)/2.
+        y_prime = 1./(1.+np.exp(-k*(x-x0)))
+
+        # now plot connectors
+        for cc in range(0,n_class):
+            UL = paths_i_ulim[cc,tt]
+            LL = paths_i_llim[cc,tt]
+            UR = paths_f_ulim[cc,tt]
+            LR = paths_f_llim[cc,tt]
+
+            L_u = UR-UL
+            upper_limit = UL + L_u*y_prime
+            L_l = LR-LL
+            lower_limit = LL + L_l*y_prime
+
+            axis.fill_between(x,lower_limit,upper_limit,color='0.5',alpha=0.25)
+            
     return 0
